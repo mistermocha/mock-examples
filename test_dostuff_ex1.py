9b63e2ee340e920b507d60be3b77da589fe0593a
Brian Weber <bweber@twitter.com>
2016-12-20 08:55:01 -0800

    docstrings

--- test_dostuff_ex1.py
+++ test_dostuff_ex1.py
@@ -1,28 +1,35 @@
-import pytest
 import mock
 
 import dostuff
 
-@pytest.fixture
-def mocked_200_request():
+'''
+Instantiating a mock is as easy as mock.Mock(). Adding fields and behaviors can then be done to the
+mocked object.
+
+Implementing return_value in a mock object makes the mock return that thing when it's executed.
+
+In the examples here, we patch requests.get to be a mock object, create additional mock objects with
+status_codes, and then assign those mocks to be the return value in pytest.
+'''
+
+
+@mock.patch('requests.get', autospec=True)
+def test_get_example_passing(mocked_get):
   mocked_req_obj = mock.Mock()
   mocked_req_obj.status_code = 200
-  return mocked_req_obj
-
-@pytest.fixture
-def mocked_400_request():
-  mocked_req_obj = mock.Mock()
-  mocked_req_obj.status_code = 400
-  return mocked_req_obj
-
-@mock.patch('requests.get', autospec=True)
-def test_get_example_passing(mocked_get, mocked_200_request):
-  mocked_get.return_value = mocked_200_request
+  mocked_get.return_value = mocked_req_obj
   assert dostuff.get_example()
+  assert mocked_get.called
+
+
+'''
+The default item returned from any mock object reference that isn't already declared is another mock
+object. The default object returned by return_value is another mock. Instead of declaring a mock,
+another method is to just directly assign properties to return_value.
+'''
 
 @mock.patch('requests.get', autospec=True)
-def test_get_example_failing(mocked_get, mocked_400_request):
-  mocked_get.return_value = mocked_400_request
+def test_get_example_failing(mocked_get):
+  mocked_get.return_value.status_code = 400
   assert not dostuff.get_example()
-
-
+  assert mocked_get.called
--- test_dostuff_ex2.py
+++ test_dostuff_ex2.py
@@ -1,7 +1,19 @@
-import pytest
 import mock
 
 import dostuff
+
+'''
+Some python actions talk to the operating system. While it's impossible to patch the complete
+filesystem, it may be deemed necessary to protect the system from actions that may slow tests or
+alter state.
+
+In this example, we patch subprocess.Popen but directly in the dostuff namespace. Depending on how
+the classes and objects are imported and instantiated, it may be necessary to patch this way.
+
+Rather than have Popen actually open a command line action, we patch Popen which will now return a
+mock. The code looks at stdout, so we return a filehandle.
+'''
+
 
 @mock.patch('dostuff.Popen')
 def test_count_the_shells(mocked_popen):
